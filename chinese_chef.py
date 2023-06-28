b979fc12e2d6275945933be0940832b5001e0502
Manish <manishdwivedi45165@gmail.com>
2023-06-28 11:26:23 -0700

    inheritance files

---
+++ chinese_chef.py
@@ -0,0 +1,7 @@
+from chef import Chef
+class ChineseChef(Chef):
+        def makes_special_dish(self):
+            print("The chef makes Manchurien")
+        def make_fried_rice(self):
+            print("The chef makes fried rice")
+    
---
+++ inheritence.py
@@ -0,0 +1,8 @@
+from chef import*
+from chinese_chef import*
+mychef = Chef()
+mychef.makes_special_dish()
+
+mychinesechef = ChineseChef()
+mychinesechef.makes_special_dish()
+mychinesechef.make_salad()
