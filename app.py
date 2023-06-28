5d6c17b3ea588392c9659efab144c65f7626a307
Manish <manishdwivedi45165@gmail.com>
2023-06-28 10:49:29 -0700

    added classes_and_object code

---
+++ app.py
@@ -0,0 +1,6 @@
+from classes import*
+
+student1 = student("Jim","Buisness",3.1 ,False)
+student2 = student("Pam","Art",2.5,True)
+print(student1.gpa)
+print(student2.name)
--- writing.py
+++ writing.py
@@ -1,3 +1,5 @@
+
+
 employee_file = open("employee.txt","a")#if we write "w" instead of "a" then it will override all content of the file 
 employee_file = open("employee1.txt","w")#this will create a new file
 employee_file.write("Toby - Human Resources")
